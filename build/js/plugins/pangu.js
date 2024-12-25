b1d1e90c96b9946d052587f53d3614ca23ba47e5
yangrenruiyrr <yangrenruiyrr@163.com>
2024-12-25 17:23:15 +0800

    Site updated: 2024-12-25 17:23:14

--- build/js/plugins/pangu.js
+++
@@ -1,1 +0,0 @@
-function initPanguJS(){pangu.spacingElementByClassName("markdown-body"),pangu.autoSpacingPage()}document.addEventListener("DOMContentLoaded",initPanguJS);try{swup.hooks.on("page:view",initPanguJS)}catch(n){}
